c04a2121c99ba7c3decdaee9c1d7c59e3129ccbf
Sathya Laufer <sathya@laufers.net>
2018-01-21 15:18:29 +0100

    Fixed syntax error preventing compilation

--- src/PhilipsHueCentral.cpp
+++ src/PhilipsHueCentral.cpp
@@ -965,7 +965,7 @@ void PhilipsHueCentral::searchHueBridges()
 			PVariable info = device.second.info();
 			if(info->structValue->find("manufacturer") == info->structValue->end() || info->structValue->find("modelName") == info->structValue->end() || info->structValue->find("serialNumber") == info->structValue->end()) continue;
 			if(info->structValue->at("manufacturer")->stringValue != "Royal Philips Electronics" || info->structValue->at("modelName")->stringValue.compare(0, 18, "Philips hue bridge") != 0) continue;
-			Systems::PPhysicalInterfaceSettings settings = std::make_shared<Systems::PhysicalInterfaceSettings>());
+			Systems::PPhysicalInterfaceSettings settings = std::make_shared<Systems::PhysicalInterfaceSettings>();
 			settings->id = BaseLib::HelperFunctions::stringReplace(info->structValue->at("serialNumber")->stringValue, ".", "-"); //Points are not allowed as they are needed for seperation of config settings
 			foundInterfaces.insert(settings->id);
 			settings->host = device.second.ip();
